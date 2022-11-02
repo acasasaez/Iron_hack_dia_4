a373a005bc84eaa53a9b7a58120ed8215ac52f4d
acasasaez <acasasae@myuax.com>
2022-11-02 10:00:31 +0100

    saxon

--- vikingClasses.py
+++ vikingClasses.py
@@ -24,3 +24,15 @@ class Viking(Soldier):
 
     def battleCry(self):
         return "Odin Owns You All!"
+
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return "A Saxon has received " + str(damage) + " points of damage"
+        else:
+            return "A Saxon has died in combat"
