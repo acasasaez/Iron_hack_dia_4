aa7baeb2be9471c07b91b5d977fcf93091a6b051
acasasaez <acasasae@myuax.com>
2022-11-02 10:08:45 +0100

    Clase war y Tests

--- vikingClasses.py
+++ vikingClasses.py
@@ -36,3 +36,40 @@ class Saxon(Soldier):
             return "A Saxon has received " + str(damage) + " points of damage"
         else:
             return "A Saxon has died in combat"
+
+class War():
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+    def addViking(self, Viking):
+        self.vikingArmy.append(Viking)
+
+    def addSaxon(self, Saxon):
+        self.saxonArmy.append(Saxon)
+
+    def vikingAttack(self):
+        import random
+        saxon = random.choice(self.saxonArmy)
+        viking = random.choice(self.vikingArmy)
+        result = saxon.receiveDamage(viking.strength)
+        if saxon.health <= 0:
+            self.saxonArmy.remove(saxon)
+        return result
+
+    def saxonAttack(self):
+        import random
+        saxon = random.choice(self.saxonArmy)
+        viking = random.choice(self.vikingArmy)
+        result = viking.receiveDamage(saxon.strength)
+        if viking.health <= 0:
+            self.vikingArmy.remove(viking)
+        return result
+
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return "Vikings have won the war of the century!"
+        elif len(self.vikingArmy) == 0:
+            return "Saxons have fought for their lives and survive another day..."
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
